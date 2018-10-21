1c5b980ac5bb14f7cc3d8e4930c6fbd9120384da
Gösta Ostendorf <gostendorf@zalando.de>
2018-10-21 15:41:38 +0200

    presentional component for item

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../static/css/ItemList.css';
+import Item from './Item';
 
 const ItemList = (props) => {
   const items = props.items;
   return (<div>
     <p className="items">Items</p>
     <ol className="item-list">
-      {items.map((item, index) => <li key={index}>{item}</li>)}</ol>
+      {items.map((item, index) => <Item key={index} item={item}/>)}</ol>
   </div>);
 }
 
